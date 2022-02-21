2ca7c20b40e2fc203f892f26d9ef27345046ad66
Raúl Toscano Miranda <rtscmrn@gmail.com>
2022-02-21 17:45:43 -0500

    testing
    teting

---
+++ Cap1/testing01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 28 10:56:32 2022
+
+@author: raultoscanomiranda
+"""
+
+import random
+random.seed()
+x= [random.uniform(-100, 100) for i in range(10)]
+y= [random.uniform(-100, 100) for i in range(10)]
+
+print("End")
